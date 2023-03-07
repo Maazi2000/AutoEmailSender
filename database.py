618c36d655a6c86cc23d530faee3902faa3e910b
Adam Mazurek <adammaz00@o2.pl>
2023-03-07 18:37:23 +0100

    addition class Database

--- database.py
+++ database.py
@@ -1,5 +1,20 @@
-import sqlite3
+'''Class that will be used to connect to the database'''
 
-with sqlite3.connect("Database.db") as connection:
-    cursor = connection.cursor()
-    cursor.execute()
+class Database:
+    # Initializing the class with a connection object
+    def __init__(self, connection):
+        self.connection = connection
+        self.cursor = None
+
+    def __enter__(self):
+        self.cursor = self.connection.cursor()
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        # Method that will handle the closing and committing/rolling back the transactions
+        if isinstance(exc_value, Exception):
+            self.connection.rollback()
+        else:
+            self.connection.commit()
+
+        self.connection.close()
